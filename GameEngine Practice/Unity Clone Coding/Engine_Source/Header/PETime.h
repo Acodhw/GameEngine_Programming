d2c4f25fdaf6abff65809ae1507bcab205e3a5dc
ACODHW <66577471+Acodhw@users.noreply.github.com>
2025-07-26 08:08:07 +0900

    TimeClass Add

---
+++ GameEngine Practice/Unity Clone Coding/Engine_Source/Header/PETime.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "CommonInclude.h"
+
+namespace PracticeEngine {
+
+	class Time
+	{
+	public:
+		static void Initailze();
+		static void Update();
+
+		static const float& DeltaTime;
+
+	private:
+		static LARGE_INTEGER cpuFrequency; //cpu 고유진동수 
+		static LARGE_INTEGER prevFrequency; //진동수 변경을 체크하기 위한 진동수
+		static LARGE_INTEGER currentFrequency; // 프로그램 시작시 현재 진동수
+		static float deltaTime; // 실제 시간 변화율
+	};
+}
+
