c6bcd4dcb4408d50e8473fe1fdf7946a391a7a6c
ACODHW <66577471+Acodhw@users.noreply.github.com>
2025-07-27 07:19:15 +0900

    BackBuffer Add

--- GameEngine Practice/Unity Clone Coding/Engine_Source/Header/PEApplication.h
+++ GameEngine Practice/Unity Clone Coding/Engine_Source/Header/PEApplication.h
@@ -8,22 +8,32 @@ namespace PracticeEngine {
 	public:
 		Application();
 		~Application();
-		void Initialize(HWND hwmd);
+		void Initialize(HWND hwmd, UINT width, UINT height);
 		void Run();
 
 		void Update();
 		void LateUpdate();
 
 		void Render();
-		void InfoTitle(); // 타이틀에 인포 표시
+
+	private:
+		void infoTitle(); // 타이틀에 인포 표시
 
 	private:
 		HWND mHwmd; // 맴버 변수 핸들
 		HDC mHdc; // hdc 가져옴
 
+		HDC mBackHDC; // 후면 버퍼에 적용할 HDC
+		HBITMAP mBackBuffer; // 그림 그릴 버퍼
+
+		UINT mWidth;
+		UINT mHeight;
+
+		wchar_t* title; // 타이틀 저장
+
 		// 플레이어
 		GameObject mPlayer; // 게임오브젝트 연동
-		wchar_t * title; // 타이틀 저장
+		
 	};
 }
 
--- GameEngine Practice/Unity Clone Coding/Engine_Source/Source/PEApplication.cpp
+++ GameEngine Practice/Unity Clone Coding/Engine_Source/Source/PEApplication.cpp
@@ -9,6 +9,10 @@ namespace PracticeEngine {
 	Application::Application()
 		: mHwmd(nullptr)
 		, mHdc(nullptr)
+		, mWidth(0)
+		, mHeight(0)
+		, mBackHDC(nullptr)
+		, mBackBuffer(nullptr)
 	{
 		title = new wchar_t[MAX_LOADSTRING];		
 	}
@@ -16,10 +20,29 @@ namespace PracticeEngine {
 	Application::~Application() {
 		delete[] title;
 	}
-	void Application::Initialize(HWND hwmd) {
+	void Application::Initialize(HWND hwmd, UINT width, UINT height) {
 		mHwmd = hwmd;
 		mHdc = GetDC(mHwmd);
+		
+		RECT rect = { 0, 0, width, height };
+		AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false); // 윈도우 작업영역을 Rect로 변경(&RECT, 윈도우스타일, 메뉴바)
+		
+		mWidth = rect.right - rect.left;
+		mHeight = rect.bottom - rect.top;
+
+		SetWindowPos(mHwmd, nullptr, 0, 0, rect.right - rect.left, rect.bottom - rect.top, 0);
+		ShowWindow(mHwmd, true);
+
 		GetWindowText(mHwmd, title, MAX_LOADSTRING);
+
+		// 윈도우 해상도에 맞는 백버퍼 생성
+		mBackBuffer = CreateCompatibleBitmap(mHdc, width, height);
+
+		//백버퍼 가르킬 dc 생성
+		mBackHDC = CreateCompatibleDC(mHdc);
+
+		HBITMAP oldBitmap = (HBITMAP)SelectObject(mBackHDC, mBackBuffer); // Buffer에다가 DC 바꿔주고
+		DeleteObject(oldBitmap); // 이전 DC는 삭제
 
 		mPlayer.SetPosition(0, 0);
 		Input::Initailze();
@@ -44,11 +67,16 @@ namespace PracticeEngine {
 	}
 
 	void Application::Render() {
-		
-		mPlayer.Render(mHdc);
+		Rectangle(mBackHDC, -5, -5, mWidth + 5, mHeight + 5);
+
+
+		mPlayer.Render(mBackHDC);
+
+		// BackBuffer를 원본  Buffer에 복사
+		BitBlt(mHdc, 0, 0, mWidth, mHeight, mBackHDC, 0, 0, SRCCOPY); 
 	}
 
-	void Application::InfoTitle() {
+	void Application::infoTitle() {
 		float fps = 1.0f / Time::DeltaTime;	
 
 		wchar_t str[MAX_LOADSTRING] = L"";
